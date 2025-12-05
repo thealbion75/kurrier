1eb0e6e723205d41184207c95a9ae1495c63c3b7
thealbion75 <eddie@thevermeers.co.uk>
2025-12-05 00:09:36 +0000

    Update import statement to include file extension

--- apps/worker/lib/migrations/index.ts
+++ apps/worker/lib/migrations/index.ts
@@ -1,4 +1,8 @@
+// Change this:
 import * as seed0086 from "./0.0.86/seed";
+
+// To this:
+import * as seed0086 from "./0.0.86/seed.ts";
 
 export const MIGRATION_REGISTRY: Record<string, any> = {
 	"0.0.86": seed0086,
